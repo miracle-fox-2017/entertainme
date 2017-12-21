f1207e9e26cccf45023aecfd9a096e5e84a9a981
Faris Widyantho <radenfaris@gmail.com>
2017-12-21 20:19:32 +0700

    update with version collection

--- entertain-me/controllers/entertainController.js
+++ entertain-me/controllers/entertainController.js
@@ -15,68 +15,66 @@ const entertain = (req, res) => {
   const getEntertain = async () => {
     try {
       //check version Movies
-      // const cacheMovVersion = await cache.multi().get('movieVersion').execAsync()
-      // const movieVersion = await axios.get('http://localhost:3001/movie/ver')
-      // cache.set('movieVersion', movieVersion.data.version, 'EX', 5)
-      // console.log(cacheMovVersion[0])
-      // console.log(movieVersion.data.version)
-      // if(cacheMovVersion[0].version && cacheMovVersion[0].version === movieVersion.data.version) {
-      //   //return cache -json parse
-      //   const movieRaw = await cache.multi().get('movies').execAsync()
-      //   const movies = JSON.parse(movieRaw)
-      // } else {
-      //   //return axios
-      //   console.log('here')
-      //   const movieRaw = await axios.get('http://localhost:3001/movie')
-      //   const movies = movieRaw.data
-      //   cache.set('movies', JSON.stringify(movies.data), 'EX', 5)
-      // }
+      const cacheMovVersion = await cache.multi().get('movieVersion').execAsync()
+      const movieVersion = await axios.get('http://localhost:3001/movie/ver')
+      cache.set('movieVersion', movieVersion.data.version, 'EX', 5)
+      if(cacheMovVersion[0] == movieVersion.data.version) {
+        //return cache -json parse
+        var movieRaw = await cache.multi().get('movies').execAsync()
+        var moviesz = JSON.parse(movieRaw)
+      } else {
+        //return axios
+        var movieRaw = await axios.get('http://localhost:3001/movie')
+        var movies = movieRaw.data
+        cache.set('movies', JSON.stringify(movies.data), 'EX', 5)
+      }
+      // check version TV
+      const cacheTvVersion = await cache.multi().get('tvVersion').execAsync()
+      const tvVersion = await axios.get('http://localhost:3002/tv/ver')
+      cache.set('tvVersion', movieVersion.data.version, 'EX', 5)
+      if(cacheTvVersion[0] == tvVersion.data.version) {
+        //return cache -json parse
+        var tvRaw = await cache.multi().get('tv').execAsync()
+        var tvsz = JSON.parse(tvRaw)
+      } else {
+        //return axios
+        var tvRaw = await axios.get('http://localhost:3002/tv')
+        var tvs = tvRaw.data
+        cache.set('tv', JSON.stringify(tvs.data), 'EX', 5)
+      }
 
-      //check version TV
-      // const cacheTvVersion = await cache.multi().get('tvVersion').execAsync()
-      // const tvVersion = await axios.get('http://localhost:3002/tv/ver')
-      // if(cacheTvVersion[0] == tvVersion.data.version) {
-      //   //return cache -json parse
-      //   const tvRaw = await cache.multi().get('tv').execAsync()
-      //   const tvs = JSON.parse(tvRaw)
-      // } else {
-      //   //return axios
-      //   const tvRaw = await axios.get('http://localhost:3002/tv')
-      //   const tvs = tvRaw.data
-      //   cache.set('tv', JSON.stringify(tvs.data), 'EX', 5)
-      // }
-
-      // res.json({
-      //   status: 'OK',
-      //   location: 'ENTERTAIN ME',
-      //   movies: movies
-      // })
-
-      cache.get('movies', (errMovie, cacheMovie) => {
-        cache.get('tv', async (errTv, cacheTv) => {
-          if(cacheTv && cacheMovie) {
-            let dataMovie = JSON.parse(cacheMovie)
-            let dataTv = JSON.parse(cacheTv)
-            res.json({
-              status: 'OK',
-              location: 'ENTERTAIN ME',
-              movies: dataMovie,
-              tvs: dataTv
-            })
-          } else {
-            const movies = await axios.get('http://localhost:3001/movie')
-            const tvs = await axios.get('http://localhost:3002/tv')
-            cache.set('movies', JSON.stringify(movies.data), 'EX', 5)
-            cache.set('tv', JSON.stringify(tvs.data), 'EX', 5)
-            res.json({
-              status: 'OK',
-              location: 'ENTERTAIN ME',
-              movies: movies.data,
-              tvs: tvs.data
-            })
-          }
-        })
+      res.json({
+        status: 'OK',
+        location: 'ENTERTAIN ME',
+        movies: movies || moviesz,
+        tvs: tvs || tvz
       })
+
+      // cache.get('movies', (errMovie, cacheMovie) => {
+      //   cache.get('tv', async (errTv, cacheTv) => {
+      //     if(cacheTv && cacheMovie) {
+      //       let dataMovie = JSON.parse(cacheMovie)
+      //       let dataTv = JSON.parse(cacheTv)
+      //       res.json({
+      //         status: 'OK',
+      //         location: 'ENTERTAIN ME',
+      //         movies: dataMovie,
+      //         tvs: dataTv
+      //       })
+      //     } else {
+      //       const movies = await axios.get('http://localhost:3001/movie')
+      //       const tvs = await axios.get('http://localhost:3002/tv')
+      //       cache.set('movies', JSON.stringify(movies.data), 'EX', 5)
+      //       cache.set('tv', JSON.stringify(tvs.data), 'EX', 5)
+      //       res.json({
+      //         status: 'OK',
+      //         location: 'ENTERTAIN ME',
+      //         movies: movies.data,
+      //         tvs: tvs.data
+      //       })
+      //     }
+      //   })
+      // })
 
     } catch (err) {
       res.status(500).send({
