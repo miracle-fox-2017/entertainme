fa7e706b6beca50dcb51b25532bc2779acf67326
ameliarahman <amel.rahman5@gmail.com>
2017-12-21 19:08:15 +0700

    Commit release 2

---
+++ tv/app.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+app = express(),
+MongoClient = require('mongodb').MongoClient,
+url = 'mongodb://amelia:amelia@cluster0-shard-00-00-71yp9.mongodb.net:27017,cluster0-shard-00-01-71yp9.mongodb.net:27017,cluster0-shard-00-02-71yp9.mongodb.net:27017/dbmicroservice?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+app.get('/api/tv', function (req, res) {
+MongoClient.connect(url, function (err, db) {
+  if(err){
+    console.log('not connected')
+  } else{
+    db.collection('tv').find().toArray(function(err, result){
+      if(err){
+        console.log(err)
+      }else{
+        res.json(result)
+      }
+    })
+  }
+})
+})
+
+app.listen(3002)
