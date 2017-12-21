ae136fc4dec51a5a1bbb3f4f1a410b0c15841628
Agustinus <vbagustinus@gmail.com>
2017-12-21 15:51:46 +0700

    redis

--- main-server/app.js
+++ main-server/app.js
@@ -1,13 +1,32 @@
-const app = require('express')()
-const Axios = require('axios')
+const app = require('express')(),
+      Axios = require('axios'),
+      redis = require("redis"),
+      cache = redis.createClient(),
+      responseTime = require('response-time')
 
+cache.on('error', err => {
+  console.log(`Error: ${err}`)
+})
+      
+app.use(responseTime())
 app.get('/entertainme', async (req, res) => {
   try {
     const movies = await Axios.get('http://localhost:3001/movie')
     const series = await Axios.get('http://localhost:3002/tv')
-    res.json({
-      movies: movies.data,
-      series: series.data
+    cache.get('entertainme', function(err, data) {
+      console.log(typeof data, 'INI ENTTERTAIN')
+      if (data) {
+          res.json(JSON.parse(data))
+          console.log('LAMA')
+      } else {
+        const data = {
+          movies: movies.data,
+          series: series.data
+        }
+        cache.setex('entertainme', 20, JSON.stringify(data))
+        console.log('BARU')
+        res.json(data)
+      }
     })
   } catch (err) {
     console.log(err)
