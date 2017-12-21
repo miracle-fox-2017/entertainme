7fd027b861c784447f02d644eb7d61c9c8a026d3
Faris Widyantho <radenfaris@gmail.com>
2017-12-21 16:17:43 +0700

    update async await

--- entertain-me/controllers/entertainController.js
+++ entertain-me/controllers/entertainController.js
@@ -1,17 +1,53 @@
-const getMovie = require('../helpers/getMovie')
-const getTv = require('../helpers/getTv')
+// const getMovie = require('../helpers/getMovie')
+// const getTv = require('../helpers/getTv')
+const redis = require("redis");
+const cache = redis.createClient();
+const axios = require('axios')
+
+cache.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+
+const getMovie = () => axios.get('http://localhost:3001/movie')
+const getTv = () => axios.get('http://localhost:3002/tv')
 
 const entertain = (req, res) => {
-  getMovie.movies((err, movies) => {
-    getTv.tvs((err, tvs) => {
-      res.json({
-        status: 'OK',
-        location: 'ENTERTAIN ME',
-        movies: movies,
-        tvs: tvs
+  const getEntertain = () => {
+    try {
+      cache.get('movies', (errMovie, cacheMovie) => {
+        cache.get('tv', async (errTv, cacheTv) => {
+          if(cacheTv && cacheMovie) {
+            let dataMovie = JSON.parse(cacheMovie)
+            let dataTv = JSON.parse(cacheTv)
+            res.json({
+              status: 'OK',
+              location: 'ENTERTAIN ME',
+              movies: dataMovie,
+              tvs: dataTv
+            })
+          } else {
+            const movies = await getMovie()
+            const tvs = await getTv()
+            cache.set('movies', JSON.stringify(movies.data), 'EX', 5)
+            cache.set('tv', JSON.stringify(tvs.data), 'EX', 5)
+            res.json({
+              status: 'OK',
+              location: 'ENTERTAIN ME',
+              movies: movies.data,
+              tvs: tvs.data
+            })
+          }
+        })
       })
-    })
-  })
+    } catch (err) {
+      res.status(500).send({
+        err: 'cannot get API',
+        msg: err
+      })
+    }
+  }
+  getEntertain()
 }
 
 module.exports = {
